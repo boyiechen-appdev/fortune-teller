d0f10fe52fe8a762126c93cd97a616c075352af8
boyiechen <boyie@uchicago.edu>
2023-04-11 14:17:39 +0000

    finish unlucky numbers

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -4,10 +4,20 @@ class NumbersController < ApplicationController
 
     5.times do
       giraffe = rand(1...100)
-      
+
       @zebra.push(giraffe)
     end
 
-    render({ :template => "lottery_stuff/woohoo.html.erb"})
-  end 
+    render({ :template => "lottery_stuff/woohoo.html.erb" })
+  end
+
+  def loser
+    @zebra = Array.new
+    5.times do
+      giraffe = rand(1...100)
+      @zebra.push(giraffe)
+
+      render({ :template => "lottery_stuff/ohhnoo.html.erb" })
+    end
+  end
 end
